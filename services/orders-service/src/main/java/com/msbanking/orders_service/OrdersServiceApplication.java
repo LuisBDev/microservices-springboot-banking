cd6ccb9e97abe87acfaf84d821985fd987b4383a
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2026-01-12 10:02:23 -0500

    feat: enable Feign clients in orders service application

--- services/orders-service/src/main/java/com/msbanking/orders_service/OrdersServiceApplication.java
+++ services/orders-service/src/main/java/com/msbanking/orders_service/OrdersServiceApplication.java
@@ -2,12 +2,14 @@ package com.msbanking.orders_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrdersServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrdersServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersServiceApplication.class, args);
+    }
 
 }
